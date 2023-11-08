624275f2641361d8fd30ea06f73d4c2623476466
vinealgarish <rheahui82@gmail.com>
2023-11-08 18:38:10 -0500

    Implemented movement
    Implemented some movement and added screen boundaries. Will revamp the movement in the next commit.

--- Prototype 3/Assets/Scripts/PlayerMovement.cs
+++ Prototype 3/Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey("up")) {
 
+            rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
             Debug.Log("moving up");
         }
-        else if (Input.GetKey("down"){
+        else if (Input.GetKey("down")){
 
+            rb.AddForce(Vector2.down * speed, ForceMode2D.Impulse);
             Debug.Log("moving down");
         }
         else if (Input.GetKey("right")) {
 
+            rb.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
             Debug.Log("moving right");
         }
         else if (Input.GetKey("left")) {
 
+            rb.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
             Debug.Log("moving left");
         }
         
