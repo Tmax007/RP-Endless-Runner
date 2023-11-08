1a38ad10579d8d742086a6b63798b721b4445ebb
vinealgarish <rheahui82@gmail.com>
2023-11-08 18:03:12 -0500

    Testing Key Input

--- Prototype 3/Assets/Scripts/PlayerMovement.cs
+++ Prototype 3/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    Rigidbody2D rigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("up")) {
+
+            Debug.Log("moving up");
+        }
+        else if (Input.GetKey("down"){
+
+            Debug.Log("moving down");
+        }
+        else if (Input.GetKey("right")) {
+
+            Debug.Log("moving right");
+        }
+        else if (Input.GetKey("left")) {
+
+            Debug.Log("moving left");
+        }
         
     }
 }
