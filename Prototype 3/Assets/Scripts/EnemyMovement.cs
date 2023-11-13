827a14d2b3a8c59d34fd2ad2f1a12ae7da12aaa7
Aditya Vashishtha <avtmax007@gmail.com>
2023-11-13 14:45:06 -0500

    destroy enemy timer

--- Prototype 3/Assets/Scripts/EnemyMovement.cs
+++ Prototype 3/Assets/Scripts/EnemyMovement.cs
@@ -5,78 +5,89 @@ using TMPro;
 
 public class EnemyMovement : MonoBehaviour
 {
-    //Direction, movement speed, & health
+    // Direction, movement speed, & health
     public Vector3 moveDirection;
     public float moveSpeed;
     public int health;
 
-    //String used to denote barrier that destroys enemies when they reach it
+    // String used to denote barrier that destroys enemies when they reach it
     public string destroyBarrier;
 
-    //Displays health
+    // Displays health
     TextMeshProUGUI healthDisplay;
 
-    //Reference for the player
+    // Reference for the player
     GameObject player;
     PlayerMovement playerLink;
 
-    //Reference for score UI
+    // Reference for score UI
     GameObject scoUI;
     ScoreUI getScore;
+
+    // Time to live in seconds
+    public float timeToLive = 10f;
+    private float destroyTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Gets reference for the player upon instantiating
+        // Gets reference for the player upon instantiating
         healthDisplay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
 
         player = GameObject.Find("Player");
         playerLink = player.GetComponent<PlayerMovement>();
 
-        //Gets reference for the score UI
+        // Gets reference for the score UI
         scoUI = GameObject.Find("Score UI");
         getScore = scoUI.GetComponent<ScoreUI>();
 
+        // Initialize destroy timer
+        destroyTimer = timeToLive;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movement
+        // Movement
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
 
-        //If health reaches 0, destroys itself & adds value to the player's health regen number
-        if(health <= 0)
+        // If health reaches 0, destroys itself & adds value to the player's health regen number
+        if (health <= 0)
         {
             Destroy(gameObject);
             playerLink.healthRegainNum++;
             getScore.scoreNum += 10;
         }
 
+        // Update destroy timer
+        destroyTimer -= Time.deltaTime;
+
+        // If destroy timer reaches zero, destroy the enemy
+        if (destroyTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
         healthDisplay.text = health.ToString();
     }
 
-    //Destroys itself when reaching specified barrier (NOT WORKING, SHOULD FIX FOR OPTIMIZATION)
+    // Destroys itself when reaching specified barrier
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == destroyBarrier)
         {
-            //Debug.Log("A");
             Destroy(gameObject);
         }
     }
 
-    //Takes damage when touching bullet
+    // Takes damage when touching bullet
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Bullet(Clone)")
+        if (collision.gameObject.name == "Bullet(Clone)")
         {
-            BulletMove BulletStats = collision.GetComponent<BulletMove>();
-            health = health - BulletStats.bulletDamage;
+            BulletMove bulletStats = collision.GetComponent<BulletMove>();
+            health -= bulletStats.bulletDamage;
             Debug.Log(health);
-
         }
     }
-
-
 }
