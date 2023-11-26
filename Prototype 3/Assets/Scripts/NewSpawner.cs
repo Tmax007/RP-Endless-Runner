ee2ff2862638e76dab82538cec167554f0e3c710
Aditya Vashishtha <avtmax007@gmail.com>
2023-11-26 02:28:00 -0500

    Rhythm implementation with beats

--- Prototype 3/Assets/Scripts/NewSpawner.cs
+++ Prototype 3/Assets/Scripts/NewSpawner.cs
@@ -7,18 +7,6 @@ public class NewSpawner : MonoBehaviour
 
     public GameObject enemy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name.Equals("SpawnZone"))
